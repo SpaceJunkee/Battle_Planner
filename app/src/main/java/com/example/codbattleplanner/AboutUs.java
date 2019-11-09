e894fefeed43e14c1bbd27cb3e720ba127c0abae
Kevin <ksandy@outlook.ie>
2019-11-09 21:17:56 +0000

    Added About Us Page

--- app/src/main/java/com/example/codbattleplanner/AboutUs.java
+++ app/src/main/java/com/example/codbattleplanner/AboutUs.java
@@ -3,12 +3,20 @@ package com.example.codbattleplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AboutUs extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        final EditText aboutUsParagraph = findViewById(R.id.aboutusParagraphText);
+            aboutUsParagraph.setText("We are a big bunch of gimps. \nThis app is a load of shite.\nGive us money");
+
+
     }
 }
