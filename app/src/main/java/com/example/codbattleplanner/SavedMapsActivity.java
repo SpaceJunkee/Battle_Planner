57299512aa654312e30b83d9b1d5bfec27a0e720
Kevin <ksandy@outlook.ie>
2019-11-09 17:05:30 +0000

    Initial commit

---
+++ app/src/main/java/com/example/codbattleplanner/SavedMapsActivity.java
@@ -0,0 +1,14 @@
+package com.example.codbattleplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedMapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_maps);
+    }
+}
